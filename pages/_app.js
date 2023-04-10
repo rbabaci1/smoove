837a812780c7d7fcc2210e47a9998de7823fe2a2
rbabaci1 <4rabah@gmail.com>
2023-04-10 07:20:45 -0700

    provide state to all the app with redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.scss';
+import { Provider } from 'react-redux';
+
+import { store } from '../reduxStore';
+// import Layout from '../components/Layout';
+import '../styles/globals.scss';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }

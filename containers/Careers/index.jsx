49ec5f8e90f15c20e20686c0b8e7c5f697d7a19c
rbabaci1 <4rabah@gmail.com>
2023-04-13 06:56:44 -0700

    implement and style careers container

--- containers/Careers/index.jsx
+++ containers/Careers/index.jsx
@@ -1,9 +1,22 @@
+import { AiOutlineArrowDown, AiOutlineArrowUp } from 'react-icons/ai';
 import styles from './styles.module.scss';
 
 const Careers = () => {
   return (
     <div className={styles.containerWrapper}>
-      <div className={styles.container}>Careers</div>
+      <div className={styles.container}>
+        <div className={styles.textContent}>
+          <h2>Start making money now</h2>
+          <p>
+            Be active, meet new people daily & earn up to <span>$2900</span> a
+            week!
+          </p>
+
+          <button type='submit'>Get an estimate</button>
+        </div>
+
+        <div className={styles.bgImg} />
+      </div>
     </div>
   );
 };

19f9f2c9b7e4a57a4a9337b30416d1d535a45b31
rbabaci1 <4rabah@gmail.com>
2023-04-14 06:34:27 -0700

    re-adjust spacing and typography

--- components/Carousel/index.jsx
+++ components/Carousel/index.jsx
@@ -7,10 +7,21 @@ import styles from './styles.module.scss';
 const Carousel = () => {
   return (
     <ImagesCarousel dots={false} className={styles.carousel}>
-      <Image src={fam1} alt='happy family moved in' priority />
-      <Image src={fam2} alt='happy family unpacking after moving' />
-      <Image src={truck} alt='company moving truck' />
-      <Image src={loading} alt='movers loading truck' />
+      <div className={styles.item}>
+        <Image src={fam1} alt='happy family moved in' priority />
+      </div>
+
+      <div className={styles.item}>
+        <Image src={fam2} alt='happy family unpacking after moving' />
+      </div>
+
+      <div className={styles.item}>
+        <Image src={truck} alt='company moving truck' />
+      </div>
+
+      <div className={styles.item}>
+        <Image src={loading} alt='movers loading truck' />
+      </div>
     </ImagesCarousel>
   );
 };

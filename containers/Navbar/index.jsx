942343a9e82bb5ef0774c6da4da3445c02ee0270
rbabaci1 <4rabah@gmail.com>
2023-04-14 19:39:10 -0700

    remove navbar hiding implementation

--- containers/Navbar/index.jsx
+++ containers/Navbar/index.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { AiOutlineMenu } from 'react-icons/ai';
 import { HiOutlineUser } from 'react-icons/hi';
@@ -9,37 +8,9 @@ import { Dropdown } from '@/components';
 
 function Navbar() {
   const router = useRouter();
-  const [isNavbarVisible, setIsNavbarVisible] = useState(true);
-  const [prevScrollPos, setPrevScrollPos] = useState(0);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentScrollPos = window.pageYOffset;
-
-      // Hide Navbar when scrolling down and user has scrolled past 10vh
-      if (
-        prevScrollPos > currentScrollPos &&
-        currentScrollPos > window.innerHeight * 1.1
-      ) {
-        setIsNavbarVisible(false);
-      } else {
-        setIsNavbarVisible(true);
-      }
-
-      setPrevScrollPos(currentScrollPos);
-    };
-
-    window.addEventListener('scroll', handleScroll);
-
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, [prevScrollPos]);
 
   return (
-    <div
-      className={`${styles.navWrapper} ${isNavbarVisible ? '' : styles.hidden}`}
-    >
+    <div className={styles.navWrapper}>
       <div className={styles.container}>
         <Link href='#home' className={styles.logo}>
           <h2>Smoove</h2>

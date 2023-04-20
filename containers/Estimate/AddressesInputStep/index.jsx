7aa491b3d21c0ce600f404846ecc5024ba8c6426
rbabaci1 <4rabah@gmail.com>
2023-04-19 18:35:24 -0700

    convert container bgImg into an image with absolute position

--- containers/Estimate/AddressesInputStep/index.jsx
+++ containers/Estimate/AddressesInputStep/index.jsx
@@ -1,11 +1,30 @@
+import Image from 'next/image';
+import { motion } from 'framer-motion';
+
 import { AddressesInput } from '@/components';
+import { bgImg } from '@/public/images';
 import styles from './styles.module.scss';
 
 const AddressesInputStep = () => {
   return (
     <div className={styles.container}>
+      <Image
+        src={bgImg}
+        alt='gray background with circles'
+        className={styles.bgImg}
+        priority
+      />
+
       <div className={styles.content}>
-        <h2 className={styles.title}>Pickup & dropOff addresses</h2>
+        <motion.h2
+          initial={{ y: 100, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.3 }}
+          viewport={{ once: true }}
+          className={styles.title}
+        >
+          Pickup & dropOff addresses
+        </motion.h2>
 
         <section className={styles.addresses}>
           <AddressesInput />

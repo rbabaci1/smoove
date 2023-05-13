1a6f1096ae3e83fba413e839b10bf2c8bb018d6c
rbabaci1 <4rabah@gmail.com>
2023-05-13 11:32:38 -0700

    .

--- pages/dashboard/index.jsx
+++ pages/dashboard/index.jsx
@@ -1,11 +1,7 @@
-import { useState } from 'react';
-
 import { Sidebar, WithAuth } from '@/components';
 import styles from './styles.module.scss';
 
 const Dashboard = () => {
-  const [pastMoves, setPastMoves] = useState([]);
-
   return (
     <div className={styles.dashboardWrapper}>
       <Sidebar />

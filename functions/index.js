4013b7911e09d110d3cca64e1ba4c74036d99d0d
rbabaci1 <4rabah@gmail.com>
2023-07-12 11:41:28 -0700

    implement sending texts to customers upon order confirmation

--- functions/index.js
+++ functions/index.js
@@ -2,26 +2,26 @@ const functions = require('firebase-functions');
 
 exports.sendSMSOnOrderChange = functions.firestore
   .document('users/{userId}/orders/{orderId}')
-  .onWrite((change, context) => {
+  .onWrite(async (change, context) => {
     const orderRef = change.after.ref;
     const newData = change.after.data();
     const oldData = change.before.data();
 
-    // If the document was created
+    // Check if the document was created
     if (!change.before.exists) {
       return orderRef.update({
         status: 'confirmed',
       });
     }
 
-    // If the document was deleted
+    // Check if the document was deleted
     if (!change.after.exists) {
       // Document deleted (onDelete logic)
       // Perform any necessary actions for document deletion
       return null;
     }
 
-    // If the document was updated (onUpdate logic)
+    // Document updated (onUpdate logic)
     // Example: Update status only if it has changed
     if (newData.status !== oldData.status) {
       return orderRef.update({
---
+++ pages/api/sendSms.js
@@ -0,0 +1,33 @@
+import twilio from 'twilio';
+
+const accountSid = process.env.NEXT_PUBLIC_TWILIO_ACCOUNT_SID;
+const authToken = process.env.NEXT_PUBLIC_TWILIO_AUTH_TOKEN;
+const twilioPhoneNumber = process.env.NEXT_PUBLIC_TWILIO_PHONE_NUMBER;
+
+const client = twilio(accountSid, authToken);
+
+export default async function sendSms(req, res) {
+  const { phoneNumber, message } = req.body;
+
+  if (req.method === 'POST') {
+    try {
+      const twilioResponse = await client.messages.create({
+        body: message,
+        from: twilioPhoneNumber,
+        to: phoneNumber,
+      });
+
+      res.status(200).json({
+        success: true,
+        message: 'SMS sent successfully',
+        twilioResponse,
+      });
+    } catch (error) {
+      console.error('Error sending SMS:', error);
+      res.status(500).json({ success: false, message: 'Failed to send SMS' });
+    }
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
+  }
+}

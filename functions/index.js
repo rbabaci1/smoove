66bf929c3bf6a21ea12520f03421c55a64a44b4f
rbabaci1 <4rabah@gmail.com>
2023-07-14 15:37:15 -0700

    update firebase cloud function to only fire on order creation

--- functions/index.js
+++ functions/index.js
@@ -1,33 +1,11 @@
 const functions = require('firebase-functions');
 
-exports.sendSMSOnOrderChange = functions.firestore
+exports.confirmOrderWhenPosted = functions.firestore
   .document('users/{userId}/orders/{orderId}')
-  .onWrite(async (change, context) => {
-    const orderRef = change.after.ref;
-    const newData = change.after.data();
-    const oldData = change.before.data();
+  .onCreate(snapshot => {
+    const orderRef = snapshot.ref;
 
-    // Check if the document was created
-    if (!change.before.exists) {
-      return orderRef.update({
-        status: 'confirmed',
-      });
-    }
-
-    // Check if the document was deleted
-    if (!change.after.exists) {
-      // Document deleted (onDelete logic)
-      // Perform any necessary actions for document deletion
-      return null;
-    }
-
-    // Document updated (onUpdate logic)
-    // Example: Update status only if it has changed
-    if (newData.status !== oldData.status) {
-      return orderRef.update({
-        status: 'confirmed',
-      });
-    }
-
-    return null;
+    return orderRef.update({
+      status: 'confirmed',
+    });
   });

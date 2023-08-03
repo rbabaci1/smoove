7385fc4c319936b33b81dfa4a1fbcffff52be505
rbabaci1 <4rabah@gmail.com>
2023-08-03 15:08:01 -0700

    .

--- containers/CustomersReviews/index.jsx
+++ containers/CustomersReviews/index.jsx
@@ -118,7 +118,7 @@ const CustomersReviews = ({ bgColor = '#f7faff' }) => {
         <section className={`${styles.reviews} ${styles.firstReviews}`}>
           <Marquee
             direction='right'
-            speed={25}
+            speed={20}
             gradientWidth={40}
             gradientColor={[247, 250, 255]}
             className={styles.marquee}
@@ -132,7 +132,7 @@ const CustomersReviews = ({ bgColor = '#f7faff' }) => {
         <section className={styles.reviews}>
           <Marquee
             direction='left'
-            speed={25}
+            speed={20}
             gradientWidth={40}
             gradientColor={[247, 250, 255]}
             className={styles.marquee}
@@ -146,7 +146,7 @@ const CustomersReviews = ({ bgColor = '#f7faff' }) => {
         <section className={styles.reviews}>
           <Marquee
             direction='right'
-            speed={25}
+            speed={20}
             gradientWidth={40}
             gradientColor={[247, 250, 255]}
             className={styles.marquee}
